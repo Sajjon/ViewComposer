e2099d85a9c8206f0d429705748742d6d20d0914
sajjon <alex.cyon@gmail.com>
2017-06-26 13:33:00 +0200

    Fixed bug where importing of ViewComposer in stencil did not work, now it works setting the argument `importViewComposerInTemplate` to true. Added new ControlStates: `Selected`, `Focused`. `Application`, `Reserved` Made it possible to only pass in titleColor to ControlState and if title (String) was passed in for state `Normal` then that title will be used for other states.

--- Source/Classes/ViewAttribute/AttributedValues/ControlState.swift
+++ Source/Classes/ViewAttribute/AttributedValues/ControlState.swift
@@ -23,6 +23,11 @@ extension ControlState {
         self.init(title: title, titleColor: nil, image: nil)
     }
     
+    /// Use together with some other state with title. Title from that other state will be copied over by ViewComposer
+    public init(_ titleColor: UIColor) {
+        self.init(title: nil, titleColor: titleColor, image: nil)
+    }
+    
     public init(_ image: UIImage) {
         self.init(title: nil, titleColor: nil, image: image)
     }
@@ -70,6 +75,62 @@ public struct Highlighted: ControlState {
 
 public struct Disabled: ControlState {
     public let state: UIControlState = .disabled
+    
+    public var title: String?
+    public var titleColor: UIColor?
+    public var image: UIImage?
+    
+    public init(title: String?, titleColor: UIColor?, image: UIImage?) {
+        self.title = title
+        self.titleColor = titleColor
+        self.image = image
+    }
+}
+
+public struct Selected: ControlState {
+    public let state: UIControlState = .selected
+    
+    public var title: String?
+    public var titleColor: UIColor?
+    public var image: UIImage?
+    
+    public init(title: String?, titleColor: UIColor?, image: UIImage?) {
+        self.title = title
+        self.titleColor = titleColor
+        self.image = image
+    }
+}
+
+public struct Focused: ControlState {
+    public let state: UIControlState = .focused
+    
+    public var title: String?
+    public var titleColor: UIColor?
+    public var image: UIImage?
+    
+    public init(title: String?, titleColor: UIColor?, image: UIImage?) {
+        self.title = title
+        self.titleColor = titleColor
+        self.image = image
+    }
+}
+
+public struct Application: ControlState {
+    public let state: UIControlState = .application
+    
+    public var title: String?
+    public var titleColor: UIColor?
+    public var image: UIImage?
+    
+    public init(title: String?, titleColor: UIColor?, image: UIImage?) {
+        self.title = title
+        self.titleColor = titleColor
+        self.image = image
+    }
+}
+
+public struct Reserved: ControlState {
+    public let state: UIControlState = .reserved
     
     public var title: String?
     public var titleColor: UIColor?
