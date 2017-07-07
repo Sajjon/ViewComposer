b8df50eaf937aa1311608f747b5c2a29a07f7e98
sajjon <alex.cyon@gmail.com>
2017-07-07 10:45:13 +0200

    ExpressibleByAttributes

--- Source/Classes/Attributed/Attributed.swift
+++ Source/Classes/Attributed/Attributed.swift
@@ -20,12 +20,8 @@ public protocol AssociatedValueStrippable: Equatable, Comparable {
 
 public typealias AttributeType = AssociatedValueStrippable & AssociatedValueEnumExtractor
 
-/// Type that holds a collection of attributes used to style some `Styleable`. 
-/// This collection can be merged with another instance of it sharing the same `Attribute` associatedtype.
-/// You can also extract values associated to a certain attribute e.g. the `UIColor` associated to the attribute `backgroundColor`.
-public protocol Attributed: Collection, ExpressibleByArrayLiteral, BaseAttributed, CustomStringConvertible {
-    
-    /// `Attribute` type used to style. Needs conformancs to `AssociatedValueStrippable` and `AssociatedValueEnumExtractor` 
+public protocol ExpressibleByAttributes: BaseAttributed {
+    /// `Attribute` type used to style. Needs conformancs to `AssociatedValueStrippable` and `AssociatedValueEnumExtractor`
     /// so that we can perform merging operations and also logic such as `contains:attribute` and `value` extraction,
     /// accessing the value associated to a certain attribute. e.g. the `UIColor` associated to the attribute `backgroundColor`
     associatedtype Attribute: AttributeType
@@ -34,6 +30,12 @@ public protocol Attributed: Collection, ExpressibleByArrayLiteral, BaseAttribute
     
     /// Attributes used to style some `Styleable` type
     var attributes: [Attribute] { get }
+}
+
+/// Type that holds a collection of attributes used to style some `Styleable`. 
+/// This collection can be merged with another instance of it sharing the same `Attribute` associatedtype.
+/// You can also extract values associated to a certain attribute e.g. the `UIColor` associated to the attribute `backgroundColor`.
+public protocol Attributed: ExpressibleByAttributes, Collection, ExpressibleByArrayLiteral, CustomStringConvertible {
     
     /// Needed for conformance to `Collection`
     var startIndex: Int { get }
