50438e7877106c6eec9ced9dcc3b5457fdfc225d
Sajjon <alex.cyon@gmail.com>
2018-04-28 10:56:00 +0200

    cleaning up

--- Source/Makeable/Makeable.swift
+++ Source/Makeable/Makeable.swift
@@ -51,16 +51,23 @@ public extension Styleable where Self: Makeable, Self.SelfMakeable == Self {
     }
 }
 
-//extension Makeable {
-//    public static func makeAndStyle(_ attributes: [Self.StyleableAttribute]) -> SelfMakeable {
-//        let `self` = Self.make(attributes)
-//        if let view = self as? UIView {
-//            let style = Style.init(tuples: attributes.asTuples()) { (first, last) in last }
-//            style.install(on: view)
-//        }
-//        return self
-//    }
-//}
+extension Makeable {
+    public static func make(_ elements: Self.StyleableAttribute...) -> SelfMakeable {
+        return make(elements)
+    }
+
+    public static func make(_ attributes: [Self.StyleableAttribute]) -> SelfMakeable {
+        let style = Style.init(tuples: attributes.asTuples()) { (_, last) in last }
+        let `self` = make(style)
+        if let view = self as? UIView {
+            let style = Style.init(tuples: attributes.asTuples()) { (first, last) in last }
+            style.install(on: view)
+        }
+        return self
+    }
+
+}
+
 public protocol SubKlass: AnyObject {
     associatedtype SuperKlassType
     func asSuper() -> SuperKlassType
@@ -68,50 +75,30 @@ public protocol SubKlass: AnyObject {
 
 extension Makeable where Self: MakeableByProxy, Self.Proxy: SubKlass, Self.Proxy.SuperKlassType == SelfMakeable {
     public static func make(_ style: Style) -> SelfMakeable {
-        let `self` = Self.makeProxy(style).asSuper()
-        if let view = self as? UIView {
-            style.install(on: view)
-        }
-        return self
+        return Self.makeProxy(style).asSuper()
     }
 }
 
 extension Makeable where Self: EmptyInitializable, SelfMakeable == Self {
     public static func make(_ style: Style) -> SelfMakeable {
-        let `self` = self.init()
-        if let view = self as? UIView {
-            style.install(on: view)
-        }
-        return self
-    }
-}
-
-extension Makeable {
-    /// Allow for use of array literals statically like this `UIButton.make(.text("foo"), .textColor(.red))`
-    public static func make(_ elements: Self.StyleableAttribute...) -> SelfMakeable {
-        return make(elements)
-    }
-
-    /// Allows for use of static instatiation like this: `UIButton.make([.text("foo"), .textColor(.red)])`
-    public static func make(_ attributes: [Self.StyleableAttribute]) -> SelfMakeable {
-        let style = Style.init(tuples: attributes.asTuples()) { (_, last) in last }
-        return make(style)
+        return self.init()
     }
 }
 
 /// Makes (pun intended) it possible to write `let label: UILabel = make([.text("hi")])` notice the lack of `.` in `.make`.
+/// let label: UILabel = make([.numberOfLines(1)])
 public func make<M>(_ attributes: [M.StyleableAttribute]) -> M where M: Makeable, M.SelfMakeable == M {
     return M.make(attributes)
 }
-let label: UILabel = make([.numberOfLines(1)])
 
 /// Makes (pun intended) it possible to write `let label: UILabel = make([.text("hi")])` notice the lack of `.` in `.make`.
+/// let label: UILabel = make([.text("hi")])
 public func make<M>(_ style: M.Style) -> M where M: Makeable, M.SelfMakeable == M {
     return M.make(style)
 }
-let label2: UILabel = make([.text("hi")])
 
 /// Array literals support
-//public func make<M: Makeable>(_ attributes: M.Attribute...) -> M {
-//    return make(attributes)
-//}
+// let label: UILabel = make(.text("hi"), .numberOfLines(1))
+public func make<M>(_ attributes: M.StyleableAttribute...) -> M where M: Makeable, M.SelfMakeable == M {
+    return make(attributes)
+}
